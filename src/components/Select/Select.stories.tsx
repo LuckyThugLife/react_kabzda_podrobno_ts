9d4ed2f76345144fda63c3c5972da3650fe1ce7c
Rustam Sharipov <rustam87.sharipov@gmail.com>
2023-01-20 15:48:39 +0500

    15 - Select component

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -1,30 +1,45 @@
 import React, {useState} from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-import OnOff from "./OnOff";
+import {Select} from "./Select";
 import {action} from "@storybook/addon-actions";
 
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
     title: 'Select stories',
-    component: OnOff,
+    component: Select,
     // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
- /*   argTypes: {
-        backgroundColor: { control: 'color' },
-    },*/
+    /*   argTypes: {
+           backgroundColor: { control: 'color' },
+       },*/
 }/* as ComponentMeta<typeof Rating>;*/
 
-const callback = action("on or off clicked")
 
-export const OnMode = () => <OnOff on={true} onClick={callback}/>
-export const OffMode = () => <OnOff on={false} onClick={callback}/>
+export const WithValue = () => {
+    const [value, setValue] = useState("2")
 
-
-export const ModeChanging = () => {
-    const [value, setValue] = useState<boolean>(true)
-   return <OnOff on={value} onClick={setValue}/>
+    return <> <Select onChange={setValue}
+                      value={value} items={[
+        {value: "1", title: "Moscow"},
+        {value: "2", title: "Minsk"},
+        {value: "3", title: "Kiev"}
+    ]}/>
+    </>
 }
 
+export const WithOutValue = () => {
+    const [value, setValue] = useState(null)
+
+    return <>
+        <Select onChange={setValue}
+                value={value}
+                items={[
+                    {value: "1", title: "Moscow"},
+                    {value: "2", title: "Minsk"},
+                    {value: "3", title: "Kiev"}
+                ]}/>
+    </>
+
+}
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 // const Template: ComponentStory<typeof Rating> = (args) => <Rating {...args} />;
 
